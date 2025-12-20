22cbc15fcb3284811ec8410ab5cbb2bf224cb500
CarlosHenriqueSL <luiscarloslopes597@gmail.com>
2025-12-20 10:56:57 -0300

    Subtotal & Total methods

--- src/main/java/com/example/course/entities/OrderItem.java
+++ src/main/java/com/example/course/entities/OrderItem.java
@@ -58,6 +58,10 @@ public class OrderItem {
         this.price = price;
     }
 
+    public Double getSubTotal() {
+        return quantity * price;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
--- src/main/java/com/example/course/entities/Payment.java
+++ src/main/java/com/example/course/entities/Payment.java
@@ -2,6 +2,8 @@ package com.example.course.entities;
 
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,6 +21,7 @@ public class Payment {
     private Integer id;
     private Instant moment;
 
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
